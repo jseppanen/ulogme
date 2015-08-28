dbef663e96887093ba5b9ee1656544aacce7a839
Jarno Seppänen <jseppanen@iki.fi>
2015-08-28 16:14:39 +0300

    Log sound only when number of streams change

--- logsound.sh
+++ logsound.sh
@@ -8,14 +8,19 @@ waittime="10"
 #------------------------------
 
 mkdir -p logs
+last_status=""
 while true
 do
 	nstreams=$(pacmd list-sink-inputs|grep -c "state: RUNNING")
 
-	# number of seconds elapsed since Jan 1, 1970 0:00 UTC
-	T="$(date +%s)"
+	# write log line iff number of streams changed
+	if [[ "$last_status" != "$nstreams" ]]; then
+		# number of seconds elapsed since Jan 1, 1970 0:00 UTC
+		T="$(date +%s)"
+		logfile="logs/sound_$(python2.7 rewind7am.py).txt"
+		echo "$T $nstreams" >> $logfile
+	fi
 
-	logfile="logs/sound_$(python2.7 rewind7am.py).txt"
-	echo "$T $nstreams" >> $logfile
+	last_status="$nstreams" # swap
 	sleep "$waittime" # sleep
 done
